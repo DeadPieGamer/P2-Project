a6dfb3305214cd1ffb6bf4a7c1bbe328a7192bb0
Carolinebchr <115777972+Carolinebchr@users.noreply.github.com>
2023-02-17 14:33:19 +0100

    InputSystem etc added and scripts

---
+++ P2 Project Unity/Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private TouchControls touchControls;
+
+    public delegate void StartTouchEvent(Vector2 position, float time);
+    public event StartTouchEvent OnStartTouch;
+    public delegate void EndTouchEvent(Vector2 position, float time);
+    public event StartTouchEvent OnEndTouch;
+
+    private void Awake()
+    {
+        touchControls = new TouchControls();
+
+    }
+    private void OnEnable()
+    {
+        touchControls.Enable();
+    }
+    private void OnDisable()
+    {
+        touchControls.Disable();
+    }
+    private void Start()
+    {
+        touchControls.Touch.TouchPress.started += ctx => StartTouch(ctx);
+        touchControls.Touch.TouchPress.canceled += ctx => EndTouch(ctx);
+    }
+    private void StartTouch(InputAction.CallbackContext context )
+    {
+        Debug.Log("Touch Started" + touchControls.Touch.TouchPosition.ReadValue<Vector2>());
+        if (OnStartTouch != null) OnStartTouch(touchControls.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.startTime); 
+    }
+    private void EndTouch(InputAction.CallbackContext context)
+    {
+        Debug.Log("Touch Ended");
+        if (OnEndTouch != null) OnEndTouch(touchControls.Touch.TouchPosition.ReadValue<Vector2>(), (float)context.time);
+    }  
+}
