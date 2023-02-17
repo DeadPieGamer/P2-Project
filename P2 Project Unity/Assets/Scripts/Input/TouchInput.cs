18dfca6185697c7d191603bcf90e3f16056161ee
Carolinebchr <115777972+Carolinebchr@users.noreply.github.com>
2023-02-17 12:40:01 +0100

    Script stuff and mobile stuff

--- P2 Project Unity/Assets/Scripts/Input/TouchInput.cs
+++ P2 Project Unity/Assets/Scripts/Input/TouchInput.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TouchInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < Input.touchCount ; i++)
+        {
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.touches[i].position); //whatever i is at this point in our loop, that is the touch we want to get and for each we are getting position
+            Debug.DrawLine(Vector3.zero, touchPosition, Color.red);
+        }
     }
 }
