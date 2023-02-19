5da2cf6d4351f22935f1debec52f69725d5b939f
Carolinebchr <115777972+Carolinebchr@users.noreply.github.com>
2023-02-19 21:51:19 +0100

    Added Sound script and sound to button

---
+++ P2 Project Unity/Assets/Scripts/Sounds/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioSource audio;
+
+    public void playButton()
+    { 
+        audio.Play(); 
+    }
+
+}
+
+    
+
