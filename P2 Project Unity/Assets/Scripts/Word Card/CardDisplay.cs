c48bfc373ed9de4e53fd1c18542f1290525f2881
ChocZeChoc <115222117+ChocZeChoc@users.noreply.github.com>
2023-02-23 13:06:50 +0100

    Card

--- P2 Project Unity/Assets/Scripts/Word Card/CardDisplay.cs
+++ P2 Project Unity/Assets/Scripts/Word Card/CardDisplay.cs
@@ -8,6 +8,8 @@ public class CardDisplay : MonoBehaviour
 {
     public WordCards card;
 
+    public WordCards[] cards;
+
     public TextMeshProUGUI wordText;
     public Image wordPicture;
     public AudioSource word_audiosource;
@@ -24,16 +26,28 @@ public class CardDisplay : MonoBehaviour
         
     }
 
+    /// <summary>
+    /// Plays audio of the card using the audio clip from WordCard class
+    /// </summary>
     public void Audio_play()
     {
         word_audiosource.Play();
     }
 
+    /// <summary>
+    /// A function that loads a new card replacing the defualt values. Text, Image & Audio.
+    /// </summary>
+    /// <param name="newcard"></param>
     public void Input_newCard(WordCards newcard)
     {
         wordText.text = newcard.danish_Word;
         wordPicture.sprite = newcard.word_Picture;
         word_audiosource.clip = newcard.word_Audio;
+    }
+
+    public void Select_RandomCards()
+    {
+
     }
 }
 
--- P2 Project Unity/Assets/Scripts/Word Card/WordCards.cs
+++ P2 Project Unity/Assets/Scripts/Word Card/WordCards.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Word", menuName = "Word Card")]
-public class WordCards : ScriptableObject
+[CreateAssetMenu(fileName = "New Word", menuName = "Word Card")] //Creating an instance inside the create menu in unity inspector
+
+public class WordCards : ScriptableObject //using ScriptableObject instead of monobehavior since we're not editing a gameobject directly
 {
-    [SerializeField, Tooltip("Word of the image in Danish")] public string danish_Word;
-    [SerializeField, Tooltip("Image of the word")] public Sprite word_Picture;
+    [Header("Card Elements")]
+    [SerializeField, Tooltip("Word of the image in Danish")] public string danish_Word; 
+    [SerializeField, Tooltip("Image of the word")] public Sprite word_Picture; 
     [SerializeField, Tooltip("Pronouciation of the word")] public AudioClip word_Audio;
 
     
