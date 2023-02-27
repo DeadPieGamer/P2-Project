46412257023cd4850142b8c48233e9a1d2e04f8b
ChocZeChoc <115222117+ChocZeChoc@users.noreply.github.com>
2023-02-27 12:19:22 +0100

    Card Set Added

--- P2 Project Unity/Assets/Scripts/Word Card/CardDisplay.cs
+++ P2 Project Unity/Assets/Scripts/Word Card/CardDisplay.cs
@@ -9,18 +9,43 @@ using System.Linq;
 public class CardDisplay : MonoBehaviour
 {
     public WordCards card;
-
     public WordCards[] cards;
-
     public TextMeshProUGUI wordText;
     public Image wordPicture;
     public AudioSource word_audiosource;
 
+    public WordCards[] TestSet;
+    public WordCards[] DairySet;
+    public WordCards[] AnimalSet;
+    public WordCards[] VegetableSet;
+    public WordCards[] FruitSet;
+    public WordCards[] DrinkSet;
+    public WordCards[] BakeSet;
+    public WordCards[] AsianSet;
+    public WordCards[] CannedSet;
+    public WordCards[] SnackSet;
+    public WordCards[] CleaningSet;
+
+    private void Awake()
+    {
+        TestSet = Resources.LoadAll("WordCards_Folder/Test", typeof(WordCards)).Cast<WordCards>().ToArray();//Call Word card folder using Resouces.LoadALL("Name of the folder", typeof(scriptableobjectname))
+        DairySet = Resources.LoadAll("WordCards_Folder/Dairy", typeof(WordCards)).Cast<WordCards>().ToArray();
+        AnimalSet = Resources.LoadAll("WordCards_Folder/Dairy", typeof(WordCards)).Cast<WordCards>().ToArray();
+        VegetableSet = Resources.LoadAll("WordCards_Folder/Dairy", typeof(WordCards)).Cast<WordCards>().ToArray();
+        FruitSet = Resources.LoadAll("WordCards_Folder/Dairy", typeof(WordCards)).Cast<WordCards>().ToArray();
+        DrinkSet = Resources.LoadAll("WordCards_Folder/Dairy", typeof(WordCards)).Cast<WordCards>().ToArray();
+        BakeSet = Resources.LoadAll("WordCards_Folder/Dairy", typeof(WordCards)).Cast<WordCards>().ToArray();
+        AsianSet = Resources.LoadAll("WordCards_Folder/Dairy", typeof(WordCards)).Cast<WordCards>().ToArray();
+        CannedSet = Resources.LoadAll("WordCards_Folder/Dairy", typeof(WordCards)).Cast<WordCards>().ToArray();
+        SnackSet = Resources.LoadAll("WordCards_Folder/Dairy", typeof(WordCards)).Cast<WordCards>().ToArray();
+        CleaningSet = Resources.LoadAll("WordCards_Folder/Dairy", typeof(WordCards)).Cast<WordCards>().ToArray();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        cards = TestSet;
         Input_newCard(card);
-        cards = Resources.LoadAll("WordCards_Folder", typeof(WordCards)).Cast<WordCards>().ToArray();//Call Word card folder using Resouces.LoadALL("Name of the folder", typeof(scriptableobjectname))
         Debug.Log(cards.Length);
     }
 
@@ -53,6 +78,10 @@ public class CardDisplay : MonoBehaviour
     {
         int randomCard = Random.Range(0, cards.Length);
         Input_newCard(cards[randomCard]);
+    }
+    public void Select_CardSet()
+    {
+       
     }
 }
 
