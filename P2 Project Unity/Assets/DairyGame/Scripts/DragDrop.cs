23d3b8797d8c9b7d01c43aa09b7e69cf35550e56
Carolinebchr <dkcbch@gmail.com>
2023-03-17 12:31:21 +0100

    finished DragandDrop Scripts

--- P2 Project Unity/Assets/DairyGame/Scripts/DragDrop.cs
+++ P2 Project Unity/Assets/DairyGame/Scripts/DragDrop.cs
@@ -8,15 +8,19 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
 // code from Code Mokey on youtube, link: https://www.youtube.com/watch?v=BGr-7GZJNXg
 
 {
-    private RectTransform rectTransform;
+    private RectTransform rectTransform; // Grab our RectTransform component
+    private CanvasGroup canvasGroup; // Grabbing our Canvas Group component
 
     private void Awake()
     {
         rectTransform = GetComponent < RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
     }
     public void OnBeginDrag(PointerEventData eventData) // This one gets called when we begin dragging the item
     {
         Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = .6f; // makes the item slightly transparent whilst it is being dragged, by modifying the alpha
+        canvasGroup.blocksRaycasts = false; // sets the blocksraycast into false so that the raycast goes through this object and lands on the item slot
     }
 
     public void OnDrag(PointerEventData eventData) // This is what gets called on every frame whilst we are dragging the item and the mouse has moved
@@ -28,6 +32,8 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
     public void OnEndDrag(PointerEventData eventData) // This one gets called when we stop dragging the item
     {
         Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f; // When we finish dragging we reset the alpha back to one
+        canvasGroup.blocksRaycasts = true; // When we finish dragging we set it back to true
     }
 
     public void OnPointerDown(PointerEventData eventData) //Which will be called when the mouse is pressed, whilst on top of this object
--- P2 Project Unity/Assets/DairyGame/Scripts/ItemSlot.cs
+++ P2 Project Unity/Assets/DairyGame/Scripts/ItemSlot.cs
@@ -8,5 +8,9 @@ public class ItemSlot : MonoBehaviour, IDropHandler
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null) // this is the gameobject that is currently being dragged and do a little test to see if it is not null, 
+        {                                  // then lets take it, get the component of RectTransform and set the anchorposition to this anchored position
+            eventData.pointerDrag.GetComponent<RectTransform>() .anchoredPosition = GetComponent<RectTransform>().anchoredPosition; 
+        }                                  // With This our items should snap into position when we drop it near the itemslot
     }
 }
