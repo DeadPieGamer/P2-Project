4d02509f1ccd15bed0959448129acf5ff0eb634b
ChocZeChoc <115222117+ChocZeChoc@users.noreply.github.com>
2023-03-17 12:53:00 +0100

    ConnectCards array assigned

--- P2 Project Unity/Assets/Scripts/MiniGame/ConnectGame/Connect_Cards.cs
+++ P2 Project Unity/Assets/Scripts/MiniGame/ConnectGame/Connect_Cards.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,21 +10,24 @@ public class Connect_Cards : MonoBehaviour
     public WordCards[] usingSet;
     public HashSet<WordCards> avaiableSet = new HashSet<WordCards>();
 
+    [SerializeField] private GameObject[] avaiableLeftIndex;
+    [SerializeField] private GameObject[] avaiableRightIndex;
+
     private void Awake()
     {
         usingSet = CardSetLoader.Get_Set(SetTypes.Meat);
 
         while (avaiableSet.Count < 4)
         {
-            int randomCard = Random.Range(0, usingSet.Length);
+            int randomCard = UnityEngine.Random.Range(0, usingSet.Length);
             avaiableSet.Add(usingSet[randomCard]);
         }
 
     }
     private void Start()
     {
+
         
-        //CardDisplay.Get_NewCard();
 
     }
    
