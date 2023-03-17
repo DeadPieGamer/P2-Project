b74a3266c6d82a3eda1a197a4b72739666977ed9
ChocZeChoc <115222117+ChocZeChoc@users.noreply.github.com>
2023-03-17 14:40:59 +0100

    Sprite Display ConnectG

--- P2 Project Unity/Assets/Scripts/MiniGame/ConnectGame/Connect_Cards.cs
+++ P2 Project Unity/Assets/Scripts/MiniGame/ConnectGame/Connect_Cards.cs
@@ -1,34 +1,43 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Connect_Cards : MonoBehaviour
 {
     public CardSetLoader CardSetLoader;
-    //public CardDisplay CardDisplay;
+    //public Connect_Game Connect_Game;
     public WordCards[] usingSet;
-    public HashSet<WordCards> avaiableSet = new HashSet<WordCards>();
+    public List<WordCards> avaiableSet = new List<WordCards>();
 
     [SerializeField] private GameObject[] avaiableLeftIndex;
     [SerializeField] private GameObject[] avaiableRightIndex;
 
     private void Awake()
     {
+       
+
+    }
+    private void Start()
+    {
         usingSet = CardSetLoader.Get_Set(SetTypes.Meat);
 
         while (avaiableSet.Count < 4)
         {
             int randomCard = UnityEngine.Random.Range(0, usingSet.Length);
-            avaiableSet.Add(usingSet[randomCard]);
+
+            if(!avaiableSet.Contains(usingSet[randomCard]))
+            {
+                avaiableSet.Add(usingSet[randomCard]);
+            }
         }
 
+       for (int i = 0; i < avaiableSet.Count; i++)
+        {
+            avaiableLeftIndex[i].GetComponent<Connect_Game>().setSprite(avaiableSet[i]);
+        }
     }
-    private void Start()
-    {
-
-        
-
-    }
-   
+    
 }
--- P2 Project Unity/Assets/Scripts/MiniGame/ConnectGame/Connect_Game.cs
+++ P2 Project Unity/Assets/Scripts/MiniGame/ConnectGame/Connect_Game.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Connect_Game : MonoBehaviour
 {
-    public Image cardImage;
+    public TextMeshProUGUI wordText;
+    public Image wordPicture;
+    public AudioSource word_audiosource;
 
     private void Awake()
     {
-        cardImage = GetComponent<Image>();
+        wordPicture = GetComponent<Image>();
     }
 
-    public void setImage(Sprite sprite)
+    public void setSprite(WordCards card)
     {
-        cardImage.sprite = sprite;
+        wordPicture.sprite = card.word_Picture;
+    }
+    public void setWord(WordCards card)
+    {
+        wordText.text = card.danish_Word;
+        word_audiosource.clip = card.word_Audio;
     }
 }
