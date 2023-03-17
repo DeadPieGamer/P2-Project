489a9712ccb1d1e40d47532934d0e197603d5276
Carolinebchr <dkcbch@gmail.com>
2023-03-17 16:17:20 +0100

    something added

--- P2 Project Unity/Assets/Scenes/Minigames/Sorting/Scripts/DragAndDrop2.cs
+++ P2 Project Unity/Assets/Scenes/Minigames/Sorting/Scripts/DragAndDrop2.cs
@@ -3,16 +3,66 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DragAndDrop2 : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{ 
+    private Vector3 mOffset;
+    private float mZCoord;
+
+    private bool mIsDragging = false;
+
+    private Vector3 mStartPosition;
+
+    public string mCorrectWord;
+
+    private void OnMouseDown()
     {
-        
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+        mIsDragging = true;
+
+        mStartPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseUp()
     {
-        
+        mIsDragging = false;
+
+        if (transform.position == mStartPosition)
+        {
+            // Picture was not dropped on a word, so reset its position
+            transform.position = mStartPosition;
+        }
     }
+
+    private void OnMouseDrag()
+    {
+        if (mIsDragging)
+        {
+            transform.position = GetMouseAsWorldPoint() + mOffset;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Word") && other.gameObject.name == mCorrectWord)
+        {
+            // Snap picture to word position
+            transform.position = other.transform.position;
+        }
+        else
+        {
+            // Picture was dropped on the wrong word, so reset its position
+            transform.position = mStartPosition;
+        }
+    }
+
+    private Vector3 GetMouseAsWorldPoint()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+
 }
+
+
