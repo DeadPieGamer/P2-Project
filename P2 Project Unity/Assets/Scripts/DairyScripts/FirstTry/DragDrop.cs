d90a5ad6d1e99139c74f99aa7a78fa6bc490a40b
Carolinebchr <115777972+Carolinebchr@users.noreply.github.com>
2023-03-31 12:24:40 +0200

    Drag Drop stay where it is

--- P2 Project Unity/Assets/Scripts/DairyScripts/FirstTry/DragDrop.cs
+++ P2 Project Unity/Assets/Scripts/DairyScripts/FirstTry/DragDrop.cs
@@ -20,19 +20,19 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
     }
     public void OnBeginDrag(PointerEventData eventData) // This one gets called when we begin dragging the item
     {
+        if (CompareTag("Undrag")) return;
         Debug.Log("OnBeginDrag");
         canvasGroup.alpha = .6f; // makes the item slightly transparent whilst it is being dragged, by modifying the alpha
         canvasGroup.blocksRaycasts = false; // sets the blocksraycast into false so that the raycast goes through this object and lands on the item slot
-       
+        
     }
 
     public void OnDrag(PointerEventData eventData) // This is what gets called on every frame whilst we are dragging the item and the mouse has moved
     {
+        if (CompareTag("Undrag")) return;
         Debug.Log("OnDrag");
-        rectTransform.anchoredPosition += eventData.delta; // This field contains the movement data, which the amout that the mouse moved since the previous frame,
-                                                           // so by adding this we wont be moving our object alongside the mouse
+        rectTransform.anchoredPosition += eventData.delta; // This field contains the movement data, which the amout that the mouse moved since the previous frame,// so by adding this we wont be moving our object alongside the mouse
     }
-
     public void OnEndDrag(PointerEventData eventData) // This one gets called when we stop dragging the item
     {
         Debug.Log("OnEndDrag");
@@ -45,6 +45,7 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
         Debug.Log("OnPointerDown");
         Audio.Play();
     }
+
 }
    
 
--- P2 Project Unity/Assets/Scripts/DairyScripts/FirstTry/ItemSlot.cs
+++ P2 Project Unity/Assets/Scripts/DairyScripts/FirstTry/ItemSlot.cs
@@ -23,7 +23,10 @@ public class ItemSlot : MonoBehaviour, IDropHandler
     {
         Debug.Log("OnDrop");
         if (eventData.pointerDrag != null) // this is the gameobject that is currently being dragged and do a little test to see if it is not null, 
-        {                                  // then lets take it, get the component of RectTransform and set the anchorposition to this anchored position
+        {
+            if (CompareTag("Undrag")) return;
+
+            // then lets take it, get the component of RectTransform and set the anchorposition to this anchored position
             eventData.pointerDrag.GetComponent<RectTransform>() .anchoredPosition = GetComponent<RectTransform>().anchoredPosition; // With This our items should snap into position when we drop it near the itemslot
 
             var Items = eventData.pointerDrag.GetComponent<Items>();
@@ -34,6 +37,8 @@ public class ItemSlot : MonoBehaviour, IDropHandler
                 if (IsCorrect())
                 {
                     Debug.Log("They're the same");
+                    Items.tag = "Undrag";
+                    Debug.Log(Items.tag);   
                 }
                 else
                 {
