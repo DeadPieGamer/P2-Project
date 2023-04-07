008e65fcfd06b02c5f4bb9930640e8c6705a57e3
ChocZeChoc <115222117+ChocZeChoc@users.noreply.github.com>
2023-04-07 20:33:44 +0200

    Remove Dupes in ShopList via list instead of hashset

--- P2 Project Unity/Assets/Scripts/ShoppingList/ShopList.cs
+++ P2 Project Unity/Assets/Scripts/ShoppingList/ShopList.cs
@@ -9,7 +9,7 @@ public class ShopList : MonoBehaviour
     public CardSetLoader CardSetLoader;
     public WordCards[] usingSet;
     public WordCards[] TryOut;
-    private HashSet<WordCards> avaiableSet = new HashSet<WordCards>();
+    private List<WordCards> avaiableSet = new List<WordCards>();
     [SerializeField] private GameObject[] avaiableListIndex;
     [SerializeField] private int Setamount;
 
@@ -27,21 +27,22 @@ public class ShopList : MonoBehaviour
         setListItem(avaiableSet);
 
     }
-    private void setListItem(HashSet<WordCards> Input)
+    private void setListItem(List<WordCards> Input)
     {
-        
-        foreach(WordCards card in Input)
-        {
-            Input.CopyTo(TryOut);
-        }
+        List<WordCards> output = new List<WordCards>();
+        output = Input.Distinct().ToList();
+        Setamount = output.Count;
         for(int i=0;i<Setamount;i++)
         {
+            TryOut[i] = output[i];
             Debug.Log(TryOut[i]);
         }
+        
         RemoveNull(TryOut);
     }
     private void RemoveNull(WordCards[] input)
     {
+        
         input = input.Where(c => c != null).ToArray();
     }
     private void GetRandomSet()
