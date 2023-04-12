1b731f26ac42e6e58a7f2a8cf9ce5f8bd983ce2a
DeadPieGamer <felix@krog.dk>
2023-04-12 15:29:50 +0200

    Reload scene and load main menu in won menu functionality added

---
+++ P2 Project Unity/Assets/Scripts/General Objects&Script/WonMenuFunctions.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WonMenuFunctions : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the currently active scene
+    /// </summary>
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    /// <summary>
+    /// Loads the main game scene (Super Market)
+    /// </summary>
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
