763a7bba52b9a97bc1b266701c83ac8b5e8b7feb
DeadPieGamer <felix@krog.dk>
2023-04-17 13:55:45 +0200

    Redid the drag drop in sorting dairy game

--- P2 Project Unity/Assets/Scripts/MiniGame/DairyScripts/DragDrop.cs
+++ P2 Project Unity/Assets/Scripts/MiniGame/DairyScripts/DragDrop.cs
@@ -8,6 +8,8 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
 // code from Code Monkey on youtube, link: https://www.youtube.com/watch?v=BGr-7GZJNXg
 
 {
+    [SerializeField, Tooltip("The canvas for a Felix script")] private Canvas f_canvas;
+
     private RectTransform rectTransform; // Grab our RectTransform component
     private CanvasGroup canvasGroup; // Grabbing our Canvas Group component
     private AudioSource Audio;
@@ -26,13 +28,14 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
         canvasGroup.blocksRaycasts = false; // sets the blocksraycast into false so that the raycast goes through this object and lands on the item slot
         
     }
-
+    /*
     public void OnDrag(PointerEventData eventData) // This is what gets called on every frame whilst we are dragging the item and the mouse has moved
     {
         if (CompareTag("Undrag")) return;
         //Debug.Log("OnDrag");
         rectTransform.anchoredPosition += eventData.delta; // This field contains the movement data, which the amout that the mouse moved since the previous frame,// so by adding this we wont be moving our object alongside the mouse
     }
+    */
     public void OnEndDrag(PointerEventData eventData) // This one gets called when we stop dragging the item
     {
         //Debug.Log("OnEndDrag");
@@ -46,6 +49,23 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
         Audio.Play();
     }
 
+    /// <summary>
+    /// Felix' attempt at a drag-drop script. By Sunny Valley Studio: https://www.youtube.com/watch?v=sXTAzcxNqv0
+    /// </summary>
+    /// <param name="data"></param>
+    public void OnDrag(PointerEventData eventData) // This is what gets called on every frame whilst we are dragging the item and the mouse has moved
+    {
+        if (CompareTag("Undrag")) return;
+
+        Vector2 position;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            (RectTransform)f_canvas.transform,
+            eventData.position,
+            f_canvas.worldCamera,
+            out position);
+
+        transform.position = f_canvas.transform.TransformPoint(position);
+    }
 }
    
 
