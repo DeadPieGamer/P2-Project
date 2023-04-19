6d87d1114133de61b60838e2bbe14389d8773114
Carolinebchr <dkcbch@gmail.com>
2023-04-19 13:12:02 +0200

    disabled buttons that came onto the wonmenu

--- P2 Project Unity/Assets/Scripts/MiniGame/DairyScripts/Sorting_PointChecker.cs
+++ P2 Project Unity/Assets/Scripts/MiniGame/DairyScripts/Sorting_PointChecker.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sorting_PointChecker : MonoBehaviour
 {
     public int points;
+    public GameObject exit;
+    public GameObject dictionary;
+    public GameObject settings;
 
     public GameObject wonMenu;
     // Start is called before the first frame update
@@ -21,11 +25,15 @@ public class Sorting_PointChecker : MonoBehaviour
             Debug.Log("You won");
             wonMenu.SetActive(true);
             points = 0;
+            DisableButton();
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisableButton()
     {
+            exit.gameObject.SetActive(false);
+            dictionary.gameObject.SetActive(false);
+            settings.gameObject.SetActive(false);
     }
+  
 }
